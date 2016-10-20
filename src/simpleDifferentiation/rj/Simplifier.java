fe63f81099757c4ada0f754958b214e0eb3ebf90
Ryan <ryanjung93@gmail.com>
2016-10-19 23:53:23 -0700

    Removed Symja from project since it did not simplify expressions properly

--- src/simpleDifferentiation/rj/Simplifier.java
+++ src/simpleDifferentiation/rj/Simplifier.java
@@ -1,10 +1,7 @@
 package simpleDifferentiation.rj;
 
-import org.matheclipse.core.eval.EvalEngine;
-import org.matheclipse.core.interfaces.IExpr;
 /**
- * Uses Symja - Java Computer Algebra Library (https://bitbucket.org/axelclk/symja_android_library/wiki/Home)
- * for to simplify an expression. 
+ * Does basic sign simplification
  * @author Ryan Jung
  *
  */
@@ -18,16 +15,15 @@ public final class Simplifier {
 	 */
 	public static String simplify(String expr){
 		expr = minusMinusToPlus(expr);
-		EvalEngine engine = new EvalEngine(true);
-		IExpr result = engine.evaluate(expr);
-		return result.toString();
+		expr = plusMinusToMinus(expr);
+		return expr;
 	}
 	/**
 	 * Changes all occurrences of a "--" in the expression to a "+" 
 	 * @param expression
 	 * @return 
 	 */
-	private static String minusMinusToPlus(String expression){
+	public static String minusMinusToPlus(String expression){
 		String result = "";
 		for(int i = 0; i < expression.length(); i++){
 			int indexToCheck = expression.indexOf("-", i);
@@ -37,6 +33,22 @@ public final class Simplifier {
 			else if(indexToCheck != -1 && expression.charAt(indexToCheck + 1) == '-'){
 				result += "+";
 				i = indexToCheck + 1;
+			}else{
+				result += expression.charAt(i);
+			}
+		}
+		return result;
+	}
+	private static String plusMinusToMinus(String expression){
+		String result = "";
+		for(int i = 0; i < expression.length(); i++){
+			int indexToCheck = expression.indexOf("+", i);
+			if(i < indexToCheck && indexToCheck != -1){
+				result += expression.charAt(i);
+			}
+			else if( indexToCheck != -1 && expression.charAt(indexToCheck+1) == '-'){
+				result += '-';
+				i = indexToCheck +1;		
 			}else{
 				result += expression.charAt(i);
 			}
